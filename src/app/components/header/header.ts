0ac1b6c3d6bf9968abf5ed2771929757e4f7647c
Milutin <eagle.it@airmontenegro.com>
2025-06-10 15:57:50 +0200

    Adds general style

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  dropdownOpen = false;
 
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  logout() {
+    // Your logout logic here
+    console.log('Logging out...');
+    this.dropdownOpen = false;
+  }
 }
